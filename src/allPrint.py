0cf6bcca7fb58622959ad94493243da46ccec244
sna1lBoy <133536934+sna1lBoy@users.noreply.github.com>
2024-03-11 18:34:42 -0500

    fixed multiple bugs

--- src/allPrint.py
+++ src/allPrint.py
@@ -1,19 +1,25 @@
+pythonPrint = print
+
 # javascript's console.log
 class console:
     def log(self):
-        print(self)
+        pythonPrint(self)
 
 # java's System.out.println
 class System:
     class out:
         def println(self):
-            print(self)
+            pythonPrint(self)
 
 # C#'s Console.write
 class Console:
     def write(self):
-        print(self)
+        pythonPrint(self)
 
 # C's printf
 def printf(self):
-    print(self)
+    pythonPrint(self)
+
+# making print better
+def print(this):
+    print(this)
